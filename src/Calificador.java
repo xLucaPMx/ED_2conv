fd73f47a4a48164c8697cdf83b18cfeb70a5ff63
Luca <1352653@alu.murciaeduca.es>
2025-06-19 17:48:55 +0200

    Método getAprobadoRef actualizado y funcional, código refactorizado

--- src/Calificador.java
+++ src/Calificador.java
@@ -26,22 +26,25 @@ public class Calificador {
         }
         return aprobado;
     }
-//    public static boolean getAprobadoRef(double c1, double t1, double e1, double c2, double t2, double e2){
-//        boolean aprobado = false;
-//        double eva1 = c1 * 0.1 + t1 * 0.3 + e1 * 0.6;
-//        double eva2 = c2 * 0.1 + t2 * 0.3 + e2 * 0.6;
-//
-//        if (eva1 > 5 || eva2 > 5){
-//            aprobado = true;
-//        }
-//        return aprobado;
-//    }
+    public static boolean getAprobadoRef(double c1, double t1, double e1, double c2, double t2, double e2){
+        boolean aprobado = true;
+        double eva1 = c1 * 0.1 + t1 * 0.3 + e1 * 0.6;
+        double eva2 = c2 * 0.1 + t2 * 0.3 + e2 * 0.6;
+
+        if (e1 < 5 || e2 < 5){
+            aprobado = false;
+        }
+        if (eva1 < 5 || eva2 < 5){
+            aprobado = false;
+        }
+        return aprobado;
+    }
     public static void main(String[] args) {
         // Ejemplo de uso
-        double c1 = 6.0, t1 = 7.0, e1 = 4.0;
-        double c2 = 5.0, t2 = 6.0, e2 = 9.0;
+        double c1 = 6.0, t1 = 4.0, e1 = 5.0;
+        double c2 = 5.0, t2 = 6.0, e2 = 6.0;
 
-        boolean resultado = getAprobado(c1, t1, e1, c2, t2, e2);
+        boolean resultado = getAprobadoRef(c1, t1, e1, c2, t2, e2);
         System.out.println("¿El estudiante está aprobado? " + resultado);
     }
 }
